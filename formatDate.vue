eddff0d67afe957a050e2070e642ef8143b6e4ec
omeripek <50427567+omeripek@users.noreply.github.com>
2022-04-12 21:09:22 +0300

    formate date

---
+++ formatDate.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <p>Heute ist der {{ date | formatDate }}</p>
+        <p>Heute ist der {{ date | formatDate("tr-TR") }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    filters: {
+        formatDate(input, format) {
+            return input.toLocaleDateString(format || "tr");
+        }
+    },
+    data() {
+        return {
+            date: new Date()
+        };
+    }
+};
+</script>
+
+<style></style>
